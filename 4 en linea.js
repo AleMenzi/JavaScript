a983d066b377751f7883aed6f2a3eb53f498c183
Alejandro Menzi <hectoralejandro_95@hotmail.com>
2023-03-12 20:23:25 -0300

    Juego de 4 en línea, creado

---
+++ 4 en linea.js
@@ -0,0 +1,67 @@
+
+function saludarYPedirNombre (){
+    alert("Bienvenido a 4 en linea. Se necesitan 2 jugadores para poder jugar")
+    console.log("Bienvenido a 4 en linea.");
+    nombreJug1 = parseInt(prompt("Ingrese nombre del primer jugador"))
+    alert("Bienvenido jugador 1 se le asignó el color Verde")
+    nombreJug2 = parseInt(prompt("Ingrese nombre del segundo jugador"))
+    alert("Bienvenido jugador 2 se le asignó el color Azul") 
+}
+saludarYPedirNombre();
+
+console.log("Jugador 1 usted es el jugador color Verde");
+console.log("Jugador 2 usted es el jugador color Azul");
+
+let turno = 0;
+const tablero = [];
+
+const presionarBoton = (press, pos) =>{
+    turno ++;
+    const boton = press.target;
+    const color = turno % 2 ? 'Green' : 'Blue'
+    boton.style.backgroundColor = color;
+    tablero[pos] = color;
+    if (ustedGano())alert("Felicitaciones Jugador " + color);
+}
+
+const ustedGano = () => {
+    //Derecha a Izquierda
+    if(tablero[0] == tablero[1] && tablero[0] == tablero[2] && tablero [0] == tablero[3] && tablero[0]){
+        return true;
+    }else if(tablero[1] == tablero[2] && tablero[1] == tablero[3] && tablero [1] == tablero[4] && tablero[1]){
+        return true;
+    } else if(tablero[5] == tablero[6] && tablero[5] == tablero[7] && tablero [5] == tablero[8] && tablero[5]){
+        return true;
+    } else if(tablero[6] == tablero[7] && tablero[6] == tablero[8] && tablero [6] == tablero[9] && tablero[6]){
+        return true;
+    }else if(tablero[10] == tablero[11] && tablero[10] == tablero[12] && tablero [10] == tablero[13] && tablero[10]){
+        return true;
+    }else if(tablero[11] == tablero[12] && tablero[11] == tablero[13] && tablero [11] == tablero[14] && tablero[11]){
+        return true;
+    } else if(tablero[15] == tablero[16] && tablero[15] == tablero[17] && tablero [15] == tablero[18] && tablero[15]){
+        return true;
+    } else if(tablero[16] == tablero[17] && tablero[16] == tablero[18] && tablero [16] == tablero[19] && tablero[16]){
+        return true;
+    // Arriba a Abajo
+    }  else if(tablero[0] == tablero[5] && tablero[0] == tablero[10] && tablero [0] == tablero[15] && tablero[0]){
+    return true;
+    } else if(tablero[1] == tablero[6] && tablero[1] == tablero[11] && tablero [1] == tablero[16] && tablero[1]){
+        return true;
+    }else if(tablero[2] == tablero[7] && tablero[2] == tablero[12] && tablero [2] == tablero[17] && tablero[2]){
+        return true;
+    } else if(tablero[3] == tablero[8] && tablero[3] == tablero[13] && tablero [3] == tablero[18] && tablero[3]){
+        return true;
+    }  else if(tablero[4] == tablero[9] && tablero[4] == tablero[14] && tablero [4] == tablero[19] && tablero[4]){
+    return true;
+    //Diagonales
+    } else if(tablero[0] == tablero[6] && tablero[0] == tablero[12] && tablero [0] == tablero[18] && tablero[0]){
+        return true;
+    }else if(tablero[1] == tablero[7] && tablero[1] == tablero[13] && tablero [1] == tablero[19] && tablero[1]){
+        return true;
+    } else if(tablero[3] == tablero[7] && tablero[3] == tablero[11] && tablero [3] == tablero[15] && tablero[3]){
+        return true;
+    }  else if(tablero[4] == tablero[8] && tablero[4] == tablero[12] && tablero [4] == tablero[16] && tablero[4])
+    return true;
+}
+
+document.querySelectorAll('button').forEach((obj, i) => obj.addEventListener('click', (press) => presionarBoton(press, i)));
